a8b102af87da59a5c093d2fad5a4721b51d7f708
Erik Heemskerk <erikheemskerk+github@gmail.com>
2023-12-05 22:30:23 +0100

    Day 5 solution

---
+++ aoc5/Program.cs
@@ -0,0 +1,396 @@
+﻿// var input = new Input(
+//     new long[] { 79, 14, 55, 13 },
+//     new[]
+//     {
+//         new[]
+//         {
+//             new Map(50, 98, 2),
+//             new Map(52, 50, 48),
+//         },
+//         new[]
+//         {
+//             new Map(0, 15, 37),
+//             new Map(37, 52, 2),
+//             new Map(39, 0, 15),
+//         },
+//         new[]
+//         {
+//             new Map(49, 53, 8),
+//             new Map(0, 11, 42),
+//             new Map(42, 0, 7),
+//             new Map(57, 7, 4),
+//         },
+//         new[]
+//         {
+//             new Map(88, 18, 7),
+//             new Map(18, 25, 70),
+//         },
+//         new[]
+//         {
+//             new Map(45, 77, 23),
+//             new Map(81, 45, 19),
+//             new Map(68, 64, 13),
+//         },
+//         new[]
+//         {
+//             new Map(0, 69, 1),
+//             new Map(1, 0, 69),
+//         },
+//         new[]
+//         {
+//             new Map(60, 56, 37),
+//             new Map(56, 93, 4),
+//         },
+//     });
+var input = new Input(
+    new long[]
+    {
+        1972667147, 405592018, 1450194064, 27782252, 348350443, 61862174, 3911195009, 181169206, 626861593,
+        138786487, 2886966111, 275299008, 825403564, 478003391, 514585599, 6102091, 2526020300, 15491453,
+        3211013652, 546191739,
+    },
+    new[]
+    {
+        new[]
+        {
+            new Map(325190047, 421798005, 78544109),
+            new Map(4034765382, 1473940091, 137996533),
+            new Map(403734156, 658668780, 288666603),
+            new Map(2574766003, 2624114227, 17352982),
+            new Map(1931650757, 2203381572, 98211987),
+            new Map(4263596455, 2843660329, 31370841),
+            new Map(1614547845, 2641467209, 55121215),
+            new Map(3441604278, 2032673361, 170708211),
+            new Map(692400759, 563703672, 94965108),
+            new Map(2992851755, 3824700930, 114550818),
+            new Map(3957953582, 2540115966, 24844899),
+            new Map(0, 500342114, 59804107),
+            new Map(4172761915, 2577017231, 47096996),
+            new Map(2029862744, 3548344768, 52256082),
+            new Map(2620304610, 2875031170, 99567251),
+            new Map(3982798481, 3939251748, 51966901),
+            new Map(2325101894, 1616388089, 203150170),
+            new Map(3612312489, 1611936624, 4451465),
+            new Map(787365867, 0, 77461445),
+            new Map(1341614907, 4162572181, 132395115),
+            new Map(2542801978, 3468402968, 31964025),
+            new Map(223387052, 947335383, 59156225),
+            new Map(2297805420, 1819538259, 27296474),
+            new Map(2082118826, 3991218649, 171353532),
+            new Map(3374211778, 3040047171, 67392500),
+            new Map(2592118985, 1341614907, 28185625),
+            new Map(2253472358, 2495782904, 44333062),
+            new Map(4219858911, 3780963386, 43737544),
+            new Map(59804107, 560146221, 3557451),
+            new Map(3107402573, 3600600850, 64973479),
+            new Map(1669669060, 3115086052, 235712356),
+            new Map(2719871861, 1369800532, 104139559),
+            new Map(3172376052, 2301593559, 194189345),
+            new Map(2980795389, 2564960865, 12056366),
+            new Map(1905381416, 3350798408, 26269341),
+            new Map(63361558, 261772511, 160025494),
+            new Map(2824011420, 3377067749, 91335219),
+            new Map(3366565397, 3107439671, 7646381),
+            new Map(282543277, 77461445, 42646770),
+            new Map(3713793353, 2696588424, 147071905),
+            new Map(2528252064, 1877585624, 14549914),
+            new Map(3927202691, 1846834733, 30750891),
+            new Map(3665815578, 3500366993, 47977775),
+            new Map(1474010022, 1892135538, 140537823),
+            new Map(3616763954, 3665574329, 49051624),
+            new Map(2915346639, 2974598421, 65448750),
+            new Map(864827312, 120108215, 141664296),
+            new Map(3860865258, 3714625953, 66337433),
+        },
+        new[]
+        {
+            new Map(3835605444, 4098164436, 1662218),
+            new Map(682286299, 0, 63480553),
+            new Map(396476124, 2072802434, 285810175),
+            new Map(1644893571, 655614677, 162631098),
+            new Map(3625179075, 4099826654, 40627721),
+            new Map(1431211762, 1859120625, 213681809),
+            new Map(2853687843, 4140454375, 103601386),
+            new Map(1390165578, 2358612609, 41046184),
+            new Map(2405285827, 3959200676, 138963760),
+            new Map(900243562, 1478767251, 170015757),
+            new Map(3727732722, 3084190064, 107872722),
+            new Map(893948759, 1084467374, 6294803),
+            new Map(210337617, 818245775, 186138507),
+            new Map(1807524669, 63480553, 592134124),
+            new Map(825849944, 1090762177, 68098815),
+            new Map(3837267662, 3192062786, 457699634),
+            new Map(0, 1648783008, 210337617),
+            new Map(3665806796, 2405285827, 61925926),
+            new Map(1070259319, 1158860992, 319906259),
+            new Map(745766852, 1004384282, 80083092),
+            new Map(2957289229, 4244055761, 50911535),
+            new Map(2544249587, 3649762420, 309438256),
+            new Map(3008200764, 2467211753, 616978311),
+        },
+        new[]
+        {
+            new Map(1169336944, 3024036226, 46676171),
+            new Map(1263157944, 1445876546, 148868263),
+            new Map(2080390054, 2683279801, 65621604),
+            new Map(949040795, 1266013203, 61140343),
+            new Map(2146011658, 2793621589, 110265098),
+            new Map(525510412, 2618124082, 65155719),
+            new Map(2977122301, 3122211455, 179751286),
+            new Map(3885825304, 713560214, 409141992),
+            new Map(1081017627, 4124851079, 88319317),
+            new Map(590666131, 2039951828, 38597255),
+            new Map(3596454634, 1981772613, 58179215),
+            new Map(2256276756, 2078549083, 315165891),
+            new Map(1639631621, 1122702206, 143310997),
+            new Map(2858399301, 1327153546, 118723000),
+            new Map(3654633849, 3301962741, 72039868),
+            new Map(749412925, 3549292249, 161128796),
+            new Map(1412026207, 541983534, 151190298),
+            new Map(1054901322, 4268850991, 26116305),
+            new Map(1829368778, 3374002609, 175289640),
+            new Map(1216013115, 2570979253, 47144829),
+            new Map(2806900243, 3070712397, 51499058),
+            new Map(1563216505, 3760425124, 76415116),
+            new Map(1782942618, 3710421045, 29953038),
+            new Map(629263386, 2903886687, 120149539),
+            new Map(2060339013, 3740374083, 20051041),
+            new Map(3156873587, 3836840240, 52553243),
+            new Map(2693038006, 4010988842, 113862237),
+            new Map(3726673717, 2411827666, 159151587),
+            new Map(2004658418, 4213170396, 55680595),
+            new Map(1812895656, 525510412, 16473122),
+            new Map(1010181138, 2748901405, 44720184),
+            new Map(910541721, 693173832, 20386382),
+            new Map(930928103, 2393714974, 18112692),
+            new Map(3209426830, 1594744809, 387027804),
+            new Map(2571442647, 3889393483, 121595359),
+        },
+        new[]
+        {
+            new Map(555269773, 2142838063, 230952411),
+            new Map(2879443939, 2889030006, 80512763),
+            new Map(192641991, 2686257040, 106620606),
+            new Map(786222184, 967781117, 56662479),
+            new Map(3467110983, 4162792381, 132174915),
+            new Map(1955778230, 0, 505352386),
+            new Map(2461130616, 1138855522, 99691153),
+            new Map(1436321461, 1403321335, 440861327),
+            new Map(2625559119, 1024443596, 104825859),
+            new Map(1255310011, 2792877646, 96152360),
+            new Map(1877182788, 697994377, 78595442),
+            new Map(3732975066, 3467110983, 374194949),
+            new Map(842884663, 2373790474, 56459390),
+            new Map(299262597, 2430249864, 256007176),
+            new Map(3599285898, 3841305932, 133689168),
+            new Map(4107170015, 3974995100, 187797281),
+            new Map(0, 505352386, 192641991),
+            new Map(1090535351, 1238546675, 164774660),
+            new Map(2959956702, 1129269455, 9586067),
+            new Map(1351462371, 1844182662, 84859090),
+            new Map(2730384978, 1929041752, 149058961),
+            new Map(899344053, 776589819, 191191298),
+            new Map(2560821769, 2078100713, 64737350),
+        },
+        new[]
+        {
+            new Map(4103141199, 3912772142, 105835099),
+            new Map(1994281004, 833968687, 112844016),
+            new Map(4208976298, 1124841590, 85990998),
+            new Map(3756966983, 4018607241, 111390720),
+            new Map(3868357703, 1907239336, 234783496),
+            new Map(2368591640, 1210832588, 293667703),
+            new Map(882426579, 2320467318, 1111854425),
+            new Map(3064998388, 717457244, 33489309),
+            new Map(3578938096, 946812703, 178028887),
+            new Map(2107125020, 750946553, 83022134),
+            new Map(3157760738, 3432321743, 421177358),
+            new Map(717457244, 4129997961, 164969335),
+            new Map(2190147154, 2142022832, 178444486),
+            new Map(3098487697, 3853499101, 59273041),
+            new Map(2662259343, 1504500291, 402739045),
+        },
+        new[]
+        {
+            new Map(0, 1820412620, 129662806),
+            new Map(613828383, 2855382935, 55943394),
+            new Map(4004726464, 3519717349, 290240832),
+            new Map(769767991, 99996214, 1720416406),
+            new Map(3126066249, 3043992795, 475724554),
+            new Map(2490184397, 2434241003, 421141932),
+            new Map(129662806, 1950075426, 484165577),
+            new Map(689805485, 0, 79962506),
+            new Map(669771777, 79962506, 20033708),
+            new Map(3601790803, 3809958181, 402935661),
+            new Map(3043992795, 4212893842, 82073454),
+        },
+        new[]
+        {
+            new Map(1305211417, 3371927062, 89487200),
+            new Map(947159122, 0, 358052295),
+            new Map(324330151, 2021970861, 8067408),
+            new Map(332397559, 654359706, 174171341),
+            new Map(506568900, 3311893445, 60033617),
+            new Map(11065691, 828531047, 45586147),
+            new Map(3556729147, 369117986, 26689998),
+            new Map(3583419145, 395807984, 258551722),
+            new Map(2356886904, 984938593, 400606547),
+            new Map(1394698617, 874117194, 110821399),
+            new Map(566602517, 3946624826, 164852367),
+            new Map(2998901322, 2301963630, 256425441),
+            new Map(0, 358052295, 11065691),
+            new Map(3331964991, 3087129289, 34908052),
+            new Map(1505520016, 2106676497, 195287133),
+            new Map(56651838, 2819450976, 267678313),
+            new Map(3366873043, 3293025783, 18867662),
+            new Map(3385740705, 3122037341, 170988442),
+            new Map(2281795782, 2799796942, 19654034),
+            new Map(2301449816, 1966533773, 55437088),
+            new Map(3992934054, 3677118500, 118543139),
+            new Map(3255326763, 2030038269, 76638228),
+            new Map(3849868384, 3803559156, 143065670),
+            new Map(2757493451, 2558389071, 241407871),
+            new Map(3841970867, 3795661639, 7897517),
+            new Map(1700807149, 1385545140, 580988633),
+            new Map(731454884, 3461414262, 215704238),
+        }
+    }
+);
+
+long part1 = input.Seeds.Select(s => input.GetDestination(s))
+                  .Min();
+Console.WriteLine(part1);
+
+long part2 = input.SeedRanges.Select(r => input.GetMinimumDestination(r))
+                  .Min();
+Console.WriteLine(part2);
+
+record Map(long Destination, long Source, int Length)
+{
+    public long SourceStart { get; } = Source;
+    public long SourceEnd { get; } = Source + Length - 1;
+    public long DestinationStart { get; } = Destination;
+    public long DestinationEnd { get; } = Destination + Length - 1;
+}
+
+record Input(
+    long[] Seeds,
+    Map[][] MapLayers
+)
+{
+    public LongRange[] SeedRanges { get; } = GetSeedRanges(Seeds);
+    
+    public long GetDestination(long seed)
+    {
+        long needle = seed;
+
+        foreach (var layer in MapLayers)
+        {
+            Map? map = layer.FirstOrDefault(m => m.SourceStart <= needle
+                                              && m.SourceEnd >= needle);
+
+            if (map is not null)
+            {
+                needle = map.DestinationStart + (needle - map.SourceStart);
+            }
+        }
+
+        return needle;
+    }
+
+    private static LongRange[] GetSeedRanges(long[] seeds)
+    {
+        var result = new List<LongRange>(seeds.Length / 2);
+
+        for (int i = 0; i < seeds.Length; i += 2)
+        {
+            result.Add(new LongRange(seeds[i], (int) seeds[i + 1]));
+        }
+
+        return result.ToArray();
+    }
+
+    public long GetMinimumDestination(LongRange seedRange)
+    {
+        List<LongRange> sourceRanges = new() { seedRange };
+
+        foreach (var layer in MapLayers)
+        {
+            var destinationRanges = sourceRanges.SelectMany(r => SplitRangeForLayer(r, layer)).ToList();
+            sourceRanges = destinationRanges.OrderBy(r => r.Start).ToList();
+        }
+
+        return sourceRanges[0].Start;
+    }
+
+    private List<LongRange> SplitRangeForLayer(LongRange source, Map[] layer)
+    {
+        List<LongRange> result = new();
+        List<LongRange> sourceCoverage = new();
+
+        foreach (var map in layer)
+        {
+            if (map.SourceStart >= source.Start
+             && map.SourceStart <= source.End)
+            {
+                long sourceEnd = Math.Min(source.End, map.SourceEnd);
+                int length = (int) (sourceEnd - map.SourceStart + 1);
+                long offset = 0;
+                result.Add(new LongRange(map.DestinationStart + offset, length));
+                sourceCoverage.Add(new LongRange(map.SourceStart + offset, length));
+            }
+            else if (map.SourceEnd >= source.Start
+                  && map.SourceEnd <= source.End)
+            {
+                // start is known to be inside source, otherwise the previous branch would have been hit
+                int length = (int) (map.SourceEnd - source.Start + 1);
+                long offset = source.Start - map.SourceStart;
+                result.Add(new LongRange(map.DestinationStart + offset, length));
+                sourceCoverage.Add(new LongRange(map.SourceStart + offset, length));
+            }
+            else if (map.SourceStart < source.Start
+                  && map.SourceEnd > source.End)
+            {
+                long sourceStart = source.Start;
+                int length = source.Length;
+                long offset = sourceStart - map.SourceStart;
+                result.Add(new LongRange(map.DestinationStart + offset, length));
+                sourceCoverage.Add(new LongRange(map.SourceStart + offset, length));
+            }
+        }
+
+        if (!result.Any())
+        {
+            result.Add(source);
+        }
+        else
+        {
+            // add unmapped parts of source range
+            sourceCoverage.Sort((x, y) => x.Start.CompareTo(y.Start));
+
+            long previousEnd = source.Start - 1;
+
+            foreach (var range in sourceCoverage)
+            {
+                if (range.Start > previousEnd + 1)
+                {
+                    result.Add(new LongRange(previousEnd + 1, (int) (range.Start - previousEnd)));
+                }
+
+                previousEnd = range.End;
+            }
+
+            if (previousEnd < source.End)
+            {
+                result.Add(new LongRange(previousEnd + 1, (int) (source.End - previousEnd)));
+            }
+        }
+
+        return result;
+    }
+}
+
+record LongRange(long Start, int Length)
+{
+    public long End { get; } = Start + Length - 1;
+};
